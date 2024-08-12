dbef4e3d13ca542f627aa541dbbd2f3c94fe1bf0
Sihang Wang <sihangwang@mines.edu>
2024-08-12 08:49:12 -0600

    Build basic structure, now the snake can be moved by swiping.

---
+++ SnakeGame/SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by sihang wang on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
